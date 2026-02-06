893d117e51b80d3949fb90d0bd33e71835be4777
DraxonV1 <indxrapid@gmail.com>
2026-02-06 14:16:16 +0100

    added permissions to workflow and bumped version to 0.1.2 🛡️🚀

--- easygradients/__init__.py
+++ easygradients/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .core import color, gradient, random, style, bg_color, bg_gradient, rainbow, typewriter, center, box
 from .presets import gradients
